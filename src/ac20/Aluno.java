002ce64c2a3bf84889fa25279a7e7116e9a419e9
Nightmeari <105650393+Nightmeari@users.noreply.github.com>
2025-11-05 08:07:18 -0300

    Add Aluno class with properties and methods

---
+++ src/ac20/Aluno.java
@@ -0,0 +1,40 @@
+public class Aluno {
+    private String matricula;
+    private String nome;
+    private String email;
+
+    public Aluno(String matricula, String nome, String email) {
+        this.matricula = matricula;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return nome + " (Matrícula: " + matricula + ", Email: " + email + ")";
+    }
+}
