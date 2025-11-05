3f33b2cc078842bdd8edddf0548a9d584c7f21af
Nightmeari <105650393+Nightmeari@users.noreply.github.com>
2025-11-05 08:09:08 -0300

    Add Paciente class with cpf and nome attributes

---
+++ src/ac21/Paciente.java
@@ -0,0 +1,25 @@
+public class Paciente {
+    private String cpf;
+    private String nome;
+
+    public Paciente(String cpf, String nome) {
+        this.cpf = cpf;
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
